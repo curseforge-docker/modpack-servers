3ecb9a951fe4b4713b65a070ad414f8f7258e5dd
Morten Hekkvang <morten.hekkvang@gmail.com>
2021-01-23 15:40:48 +0100

    Add github workflow for docker build and push

--- build-args.sh
+++ build-args.sh
@@ -2,5 +2,3 @@ export JAVA_VERSION=8
 export MINECRAFT_VERSION=1.12.2
 export FORGE_VERSION=14.23.5.2854
 export PACK_NAME=RLCraft
-export PACK_VERSION=2.8.2
-export DOWNLOAD_URL="https://edge.forgecdn.net/files/2935/323/RLCraft Server Pack 1.12.2 - Beta v2.8.2.zip"
